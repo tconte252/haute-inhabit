45237d37fe4dc6351ae442a374a2fbeeabb132db
tconte252 <tconte252@gmail.com>
2016-04-10 18:30:34 -0400

    mobile styling

--- wp-content/themes/hauteinhabit-NEW/js/wptuts-caroufredsel.js
+++ wp-content/themes/hauteinhabit-NEW/js/wptuts-caroufredsel.js
@@ -1,8 +1,12 @@
 jQuery(function($) {
-$('#foo2').carouFredSel({
-prev: '#prev2',
-next: '#next2',
-auto: false,
-items: 3
-});
+	$('#foo2').carouFredSel({
+	prev: '#prev2',
+	next: '#next2',
+	auto: false,
+	items: 3,
+	  scroll : { 
+        items            : 1
+    } 
+
+	});
 });
